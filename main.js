084addd6f2701632e8670388c134d116cb1c2355
Pomke <pomke@pomke.com>
2015-03-18 19:30:08 +1100

    Added loading.. text

--- main.js
+++ main.js
@@ -5,14 +5,20 @@ var moment  = require('moment');
 
 var groupEventsURL = 'https://api.meetup.com/2/events?offset=0&format=json&limited_events=False&group_urlname=Code-Kuma&photo-host=public&page=20&fields=&order=time&desc=false&status=upcoming&sig_id=14309803&sig=719ee74a8f25dee4b4fda7add4021ab667d732c4';
 
-var DATA = {};
+var DATA = {
+    nextEvent : { 
+        time : "loading..", 
+        venue : { address_1 : "loading.." }
+    }
+};
+
+rivets.bind(document.body, DATA);
 
 req({
     url: groupEventsURL,
     success: function(data) { 
         DATA.nextEvent = data.results[0];
         DATA.nextEvent.time = moment(DATA.nextEvent.time).format('MMMM Do YYYY, h:mm a');
-        rivets.bind(document.body, DATA);
     }
 });
 
